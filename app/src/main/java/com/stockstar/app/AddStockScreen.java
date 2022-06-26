6b057ac42f9bd3f110ef54eb725926f3aee2c3bd
Chris <chrisdoyle20@gmail.com>
2022-06-26 18:44:05 +0100

    Stock input UI
    Added UI elements for add stock activity consisting of a Linear Layout containing four user inputs and a button that will eventually compile the input into a SQL statement

---
+++ app/src/main/java/com/stockstar/app/AddStockScreen.java
@@ -0,0 +1,39 @@
+package com.stockstar.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class AddStockScreen extends AppCompatActivity {
+
+    private Spinner locationDropdown;
+    private Button inputButton;
+    //Default string array for Spinner dropdown list in input
+    //This is to avoid user errors in manual entry
+    // TODO: create add/remove methods for this array
+    // TODO: add non-selectable hint to array
+    private String[] locationArray = new String[]{"Cupboard",
+            "Fridge", "Freezer"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_stock_screen);
+
+        //assign UI elements
+        locationDropdown = findViewById(R.id.spGroceryLocation);
+        inputButton = findViewById(R.id.btnInputStock);
+
+        //create adapter for spinner dropdown
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, locationArray);
+        //assign adapter to spinner
+        locationDropdown.setAdapter(adapter);
+    }
+
+}
--- app/src/main/java/com/stockstar/app/StockScreen.java
+++ app/src/main/java/com/stockstar/app/StockScreen.java
@@ -2,13 +2,33 @@ package com.stockstar.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StockScreen extends AppCompatActivity {
+
+    private Button addStockButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_screen);
+
+        //assign buttons
+        addStockButton = findViewById(R.id.btnAddStock);
+
+        //set button listeners
+        addStockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openAddStockScreen();}
+        });
     }
+
+    public void openAddStockScreen() {
+        Intent intent = new Intent(this, AddStockScreen.class);
+        startActivity(intent);
+    }
+
 }
